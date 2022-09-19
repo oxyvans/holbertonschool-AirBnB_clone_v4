714000e3f368e25746e92673524c041d44e65bcb
oxyvans <benja.1999.g@hotmail.com>
2022-09-19 15:35:27 +0000

    add task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   });
 });
  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status == 200) {
+    if (data.status == 'OK') {
       $('div#api_status').addClass('available');
     } else {
       $('div#api_status').removeClass('available');
