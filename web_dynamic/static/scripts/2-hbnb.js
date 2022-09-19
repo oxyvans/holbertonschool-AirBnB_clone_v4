6d1ae55a4f8f0b674c8b4999d2036a77bac07468
oxyvans <benja.1999.g@hotmail.com>
2022-09-19 14:23:57 +0000

    add task 2

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  let res = [];
+  $('input:checkbox').change(function() {
+  if ($(this).is(":checked")) {
+    const x = ($(this).attr('data-name'));
+    res.push(x);
+  } else {
+    const n = ($(this).attr('data-name'));
+    const indx = array.indexOf(n);
+    res.splice(indx, 1);
+  }
+  $('.amenities > h4').text(res.join(', '));
+  });
+});
+ $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status == 200) {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
